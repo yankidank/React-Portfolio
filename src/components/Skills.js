247c46d9ded8dbc0de18a808379ced88d3e9d086
Eric Heikkinen <18619+yankidank@users.noreply.github.com>
2020-05-28 20:07:40 -0700

    Blue button hover color changed. Added more skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,23 +11,26 @@ function Skills(props) {
 						<li>Product Management</li>
 						<li>UI/UX Design</li>
 						<li>Agile Development</li>
+						<li>JavaScript</li>
 						<li>MongoDB</li>
+						<li>Mongoose</li>
 						<li>Express.js</li>
 						<li>React.js</li>
 						<li>Node.js</li>
 						<li>JSX</li>
-						<li>Mongoose</li>
-						<li>JavaScript</li>
 						<li>Ajax</li>
 						<li>jQuery</li>
+						<li>SQL</li>
 						<li>NoSQL</li>
 						<li>MySQL</li>
+						<li>ORM</li>
 						<li>PHP</li>
 						<li>REST API</li>
 						<li>PWA</li>
 						<li>Webpack</li>
 						<li>CSS</li>
 						<li>HTML</li>
+						<li>WordPress</li>
 						<li>GitHub</li>
 						<li>Apache</li>
 						<li>eCommerce</li>
